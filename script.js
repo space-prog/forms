a53ce87713adff645c6108ce42d6dd22d4225824
space-prog <p87717891@gmail.com>
2024-08-14 19:08:10 +0300

    checkboxes

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-const openPopup = document.querySelector(".open"),
-    popupContainer = document.querySelector(".popup"),
-    closePopup = document.querySelector(".close-popup")
-
-openPopup.addEventListener("click", function(e) {
-    e.preventDefault()
-    popupContainer.style.display = "block"
-})
-
-closePopup.addEventListener("click", function(e) {
-    e.preventDefault()
-    popupContainer.style.display = "none"
-})
+document.addEventListener("DOMContentLoaded", function() {
+    const openPopup = document.querySelector(".open"),
+        popupContainer = document.querySelector(".popup"),
+        closePopup = document.querySelector(".close-popup"),
+        checkedInput = [...document.querySelectorAll(".block3_checkboxes input")],
+        checkedSpan = [...document.querySelectorAll(".block3_checkboxes span")]
+    
+    console.log(checkedInput, checkedSpan)
+    
+    openPopup.addEventListener("click", function (e) {
+        e.preventDefault()
+        popupContainer.style.display = "block"
+    })
+    
+    closePopup.addEventListener("click", function (e) {
+        e.preventDefault()
+        popupContainer.style.display = "none"
+    })
+    
+    checkedInput.forEach((itemInput, index) => {
+        itemInput.addEventListener("click", function () {
+                if (itemInput.checked) {
+                    checkedSpan[index].style.backgroundColor = "#3e6aff"
+                } else {
+                    checkedSpan[index].style.backgroundColor = "#cfcfd1"
+                }
+            })
+        })
+    })
