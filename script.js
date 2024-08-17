21c9060c8d0844b28359491b0e60c723fe3fef58
space-prog <p87717891@gmail.com>
2024-08-17 13:24:48 +0300

    dsf

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ document.addEventListener("DOMContentLoaded", function() {
         popupContainer = document.querySelector(".popup"),
         closePopup = document.querySelector(".close-popup"),
         checkedInput = [...document.querySelectorAll(".block3_checkboxes input")],
-        checkedSpan = [...document.querySelectorAll(".block3_checkboxes span")]
+        checkedSpan = [...document.querySelectorAll(".block3_checkboxes span")],
+        checkedRadioMan = document.querySelector("#man"),
+        checkedRadioGirl = document.querySelector("#girl"),
+        tableSpan1 = document.querySelector("#tablespan1"),
+        tableSpan2 = document.querySelector("#tablespan2")
         
     
     console.log(checkedInput, checkedSpan)
@@ -20,13 +24,21 @@ document.addEventListener("DOMContentLoaded", function() {
     
     checkedInput.forEach((itemInput, index) => {
         itemInput.addEventListener("click", function () {
-                if (itemInput.checked) {
-                    checkedInput[index].style.boxShadow = "0 0 2.5px rgba(0,0,0,0.5)"
-                    checkedSpan[index].style.backgroundColor = "#3e6aff"
-                } else {
-                    checkedInput[index].style.boxShadow = "none"
-                    checkedSpan[index].style.backgroundColor = "#cfcfd1"
-                }
-            })
+            if (itemInput.checked) {
+                checkedInput[index].style.boxShadow = "0 0 2.5px rgba(0,0,0,0.5)"
+                checkedSpan[index].style.backgroundColor = "#3e6aff"
+            } else {
+                checkedInput[index].style.boxShadow = "none"
+                checkedSpan[index].style.backgroundColor = "#cfcfd1"
+            }
         })
     })
+        
+    checkedRadioMan.addEventListener("change", function() {
+        tableSpan1.style.borderColor = "#3e6aff"
+    })
+
+    checkedRadioGirl.addEventListener("change", function() {
+        tableSpan2.style.borderColor = "#3e6aff"
+    })
+})
