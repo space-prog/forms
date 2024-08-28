80959674c325fb37a622a038cd3053f3bccde0f7
pc <p87717891@gmail.com>
2024-08-28 11:03:59 +0300

    php

--- php/form1.php
+++ php/form1.php
@@ -1,3 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/php.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <div class="test">
+        <div class="wrapper1">
+            <h1 class="thanks">Ви успішно оформили замовлення!</h1>
+            <div class="circle margin"></div>
+            <p>Ми зв'яжемося з вами найближчим часом</p>
+            <div class="margin">
+                <a href="../index.html">На головну</a>
+            </div>
+        </div>
+    </div>
 <?php
 if($_POST) {
     try {
@@ -5,7 +27,7 @@ if($_POST) {
         $mail = $_POST["mail"];
         $mailInput = $_POST["mail1"];
         $select = $_POST["form1select"];
-    
+        
         } catch (\Throwable $th) {
             $name = "ERROR IN MESSAGE";
         }
@@ -29,3 +51,5 @@ if($_POST) {
         $result = file_get_contents($urlQuery);
 }
 ?>
+</body>
+</html>
